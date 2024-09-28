abd63521d63e03a01acf902470f144c3879240b6
Obemik <misaperepecaj@gmail.com>
2024-09-29 01:16:56 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Reservoir {
+private:
+	string _name;
+	double _width;  
+	double _length; 
+	double _maxdepth; 
+public:
+};
