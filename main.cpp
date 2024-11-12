74d55b63cb0c35e97f12dc83a3386b1031c359f6
Obemik <misaperepecaj@gmail.com>
2024-11-12 16:28:16 +0200

    files

--- main.cpp
+++ main.cpp
@@ -1,231 +1,7 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <fstream>
 #include <string>
 using namespace std;
-
-class FileProcessResult abstract
-{
-public:
-    virtual void get_message() = 0;
-};
-
-template<typename T>
-class FileInterface abstract
-{
-public:
-    virtual FileProcessResult* open(ios_base::openmode mode) = 0;
-    virtual FileProcessResult* close() = 0;
-    virtual FileProcessResult* write(T data, ios_base::openmode mode) = 0;
-    virtual FileProcessResult* read(T& data, ios_base::openmode mode) = 0;
-    virtual FileProcessResult* read_by_index(T& data, ios_base::openmode mode, int itemIndex = 0) = 0;
-};
-
-class FileOpenSuccess : public FileProcessResult
-{
-public:
-    FileOpenSuccess() = default;
-    void get_message() override
-    {
-        cout << "File opened successfully" << endl;
-    }
-};
-
-class FileOpenError : public FileProcessResult
-{
-public:
-    FileOpenError() = default;
-    void get_message() override
-    {
-        cout << "Error opening file" << endl;
-    }
-};
-
-class FileCloseSuccess : public FileProcessResult
-{
-public:
-    FileCloseSuccess() = default;
-    void get_message() override
-    {
-        cout << "File closed successfully" << endl;
-    }
-};
-
-class FileWriteSuccess : public FileProcessResult
-{
-public:
-    FileWriteSuccess() = default;
-    void get_message() override
-    {
-        cout << "Data written successfully" << endl;
-    }
-};
-
-class FileWriteError : public FileProcessResult
-{
-    exception e;
-public:
-    FileWriteError() = default;
-    FileWriteError(exception e)
-    {
-        this->e = e;
-    }
-    void get_message() override
-    {
-        cout << "Error writing data" << endl;
-    }
-};
-
-class FileReadSuccess : public FileProcessResult
-{
-public:
-    FileReadSuccess() = default;
-    void get_message() override
-    {
-        cout << "Data read successfully" << endl;
-    }
-};
-
-class FileReadError : public FileProcessResult
-{
-    exception e;
-public:
-    FileReadError() = default;
-    FileReadError(exception e)
-    {
-        this->e = e;
-    }
-    void get_message() override
-    {
-        cout << "Error writing data" << endl;
-    }
-};
-
-template<typename T>
-class FileService : public FileInterface<T>
-{
-    string filename;
-    fstream file;
-public:
-    FileService(string filename)
-    {
-        this->filename = filename;
-    }
-    FileProcessResult* open(ios_base::openmode mode) override
-    {
-        FileProcessResult* result = nullptr;
-        file.open(filename, mode);
-        if (file.is_open())
-        {
-            result = new FileOpenSuccess();
-        }
-        else
-        {
-            result = new FileOpenError();
-        }
-        return result;
-    }
-    FileProcessResult* close() override
-    {
-        file.close();
-        return new FileCloseSuccess();
-    }
-    FileProcessResult* write(T data, ios_base::openmode mode) override
-    {
-        try
-        {
-            FileProcessResult* result = this->open(mode);
-            result->get_message();
-            if (typeid(*result) == typeid(FileOpenSuccess))
-            {
-                if (mode == ios::app || mode == ios::out || mode == (ios::out | ios::binary) || mode == (ios::app | ios::binary))
-                {
-                    file.write((char*)&data, sizeof(T));
-                }
-                else
-                {
-                    return new FileWriteError();
-                }
-                result = this->close();
-                result->get_message();
-                return new FileWriteSuccess();
-            }
-            else if (typeid(*result) == typeid(FileOpenError))
-            {
-                return new FileWriteError();
-            }
-        }
-        catch (exception e)
-        {
-            return new FileWriteError(e);
-        }
-    }
-
-    FileProcessResult* read(T& data, ios_base::openmode mode) override
-    {
-        try
-        {
-            FileProcessResult* result = this->open(mode);
-            result->get_message();
-            if (typeid(*result) == typeid(FileOpenSuccess))
-            {
-                if (mode == ios::in || mode == (ios::in | ios::binary))
-                {
-                    file.read((char*)&data, sizeof(T));
-                }
-                else
-                {
-                    return new FileWriteError();
-                }
-                result = this->close();
-                result->get_message();
-                return new FileReadSuccess();
-            }
-            else if (typeid(*result) == typeid(FileOpenError))
-            {
-                return new FileReadError();
-            }
-        }
-        catch (exception e)
-        {
-            return new FileReadError(e);
-        }
-    }
-
-    FileProcessResult* read_by_index(T& data, ios_base::openmode mode, int itemIndex = 0) override
-    {
-        try
-        {
-            FileProcessResult* result = this->open(mode);
-            result->get_message();
-            if (typeid(*result) == typeid(FileOpenSuccess))
-            {
-                if (mode == ios::in || mode == (ios::in | ios::binary))
-                {
-                    file.seekg(itemIndex * sizeof(T), ios::beg);
-                    file.read((char*)&data, sizeof(T));
-                }
-                else
-                {
-                    return new FileWriteError();
-                }
-                result = this->close();
-                result->get_message();
-                return new FileReadSuccess();
-            }
-            else if (typeid(*result) == typeid(FileOpenError))
-            {
-                return new FileReadError();
-            }
-        }
-        catch (exception e)
-        {
-            return new FileReadError(e);
-        }
-    }
-
-};
-
 
 class Human {
 private:
@@ -234,18 +10,21 @@ private:
 public:
     Human() : fullName("Unnamed") {}
 
-    Human(const string& name) {
-        fullName = name;
-    }
-
-    Human(const Human& other) {
-        fullName = other.fullName;
-    }
-
-    ~Human() {}
+    Human(const string& name) : fullName(name) {}
 
     void printName() const {
         cout << "Name: " << fullName << endl;
+    }
+
+    friend ostream& operator<<(ostream& os, const Human& human) {
+        os << human.fullName;
+        return os;
+    }
+
+    friend istream& operator>>(istream& is, Human& human) {
+        is >> ws;
+        getline(is, human.fullName);
+        return is;
     }
 };
 
@@ -266,11 +45,6 @@ public:
         residents = new Human[capacity];
     }
 
-    Apartment(const Apartment& other) : size(other.size), capacity(other.capacity) {
-        residents = new Human[capacity];
-        copyResidents(other.residents, size);
-    }
-
     ~Apartment() {
         delete[] residents;
     }
@@ -287,10 +61,26 @@ public:
     }
 
     void printResidents() const {
-        cout << "Residents in the apartment:" << endl;
         for (size_t i = 0; i < size; i++) {
             residents[i].printName();
         }
+    }
+
+    friend ostream& operator<<(ostream& os, const Apartment& apartment) {
+        os << apartment.size << endl;
+        for (size_t i = 0; i < apartment.size; i++) {
+            os << apartment.residents[i] << endl;
+        }
+        return os;
+    }
+
+    friend istream& operator>>(istream& is, Apartment& apartment) {
+        is >> apartment.size;
+        apartment.residents = new Human[apartment.size];
+        for (size_t i = 0; i < apartment.size; i++) {
+            is >> apartment.residents[i];
+        }
+        return is;
     }
 };
 
@@ -311,11 +101,6 @@ public:
         apartments = new Apartment[capacity];
     }
 
-    Building(const Building& other) : size(other.size), capacity(other.capacity) {
-        apartments = new Apartment[capacity];
-        copyApartments(other.apartments, size);
-    }
-
     ~Building() {
         delete[] apartments;
     }
@@ -332,9 +117,31 @@ public:
     }
 
     void printApartments() const {
-        cout << "Apartments in the building:" << endl;
         for (size_t i = 0; i < size; i++) {
             apartments[i].printResidents();
+        }
+    }
+
+    void saveToFile(const string& filename) const {
+        ofstream file(filename);
+        if (file.is_open()) {
+            file << size << endl;
+            for (size_t i = 0; i < size; i++) {
+                file << apartments[i];
+            }
+            file.close();
+        }
+    }
+
+    void loadFromFile(const string& filename) {
+        ifstream file(filename);
+        if (file.is_open()) {
+            file >> size;
+            apartments = new Apartment[size];
+            for (size_t i = 0; i < size; i++) {
+                file >> apartments[i];
+            }
+            file.close();
         }
     }
 };
@@ -353,7 +160,13 @@ int main() {
     building.addApartment(apartment1);
     building.addApartment(apartment2);
 
-    building.printApartments();
+    building.saveToFile("building.txt");
+    cout << "Дані збережено у файл." << endl;
+
+    Building loadedBuilding;
+    loadedBuilding.loadFromFile("building.txt");
+    cout << "Завантажені дані з файлу:" << endl;
+    loadedBuilding.printApartments();
 
     return 0;
 }
